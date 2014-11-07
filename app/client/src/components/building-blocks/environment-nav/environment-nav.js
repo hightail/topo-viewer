9dd9d822aabc3252af9ef926043a6b58e7a58835
justin.fiedler <justin.fiedler@hightail.com>
2014-11-07 12:40:45 -0800

    Created environemnt-nav component

---
+++ app/client/src/components/building-blocks/environment-nav/environment-nav.js
@@ -0,0 +1,40 @@
+/**
+ * environment-nav Component
+ *
+ * @class EnvironmentNavComponent
+ * @module Hightail
+ * @submodule Hightail.Components
+ *
+ * @example
+ *    <ht-environment-nav></ht-environment-nav>
+ *
+ * @author justin.fiedler
+ * @since 0.0.0
+ *
+ * @copyright (c) 2014 Hightail Inc. All Rights Reserved
+ */
+'use strict';
+
+angular.wilson.component('environment-nav', {
+  scope: {
+    groupNames: '=',
+    groups: '=',
+    selectionManager: '='
+  },
+  controller: ['$scope', '$attrs', function($scope, $attrs) {
+    var controller = this;
+
+//  controller.setState({
+//    initial: '',
+//    events: [
+//      { name: '',  from: '',  to: '' }
+//    ],
+//    timeouts: [],
+//    callbacks: {}
+//  });
+  }],
+  
+  link: function($scope, $element, $attrs, controller) {
+  }
+  
+});
